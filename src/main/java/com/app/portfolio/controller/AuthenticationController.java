638239f178da65e5c6920ad16a57683d70021479
sushantAtwork <sushant.raj@netsmartz.net>
2024-10-12 02:37:15 +0530

    handled user exist exception

--- src/main/java/com/app/portfolio/controller/AuthenticationController.java
+++ src/main/java/com/app/portfolio/controller/AuthenticationController.java
@@ -1,14 +1,19 @@
 package com.app.portfolio.controller;
 
 
+import com.app.portfolio.constants.CommonConstants;
+import com.app.portfolio.constants.MessageConstant;
 import com.app.portfolio.entity.User;
 import com.app.portfolio.constants.RequestMappingConstants;
 import com.app.portfolio.dto.JwtRequest;
 import com.app.portfolio.dto.LoginResponse;
 import com.app.portfolio.dto.RegisterUserDto;
 import com.app.portfolio.helper.JwtService;
+import com.app.portfolio.model.UserResult;
+import com.app.portfolio.service.UserService;
 import com.app.portfolio.serviceImpl.authenticationServiceImpl.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +29,24 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Autowired
+    private UserService userService;
+
 
     @RequestMapping(value = RequestMappingConstants.SIGNUP, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
     public ResponseEntity<?> register(@RequestBody RegisterUserDto registerUserDto) {
-        User registeredUser = authenticationService.signup(registerUserDto);
-
-        return ResponseEntity.ok(registeredUser);
+        try{
+            User userExist = userService.findByEmail(registerUserDto.getEmail());
+            if(userExist != null){
+                return ResponseEntity.ok(new UserResult<>(null, HttpStatus.OK, MessageConstant.USER_EXIST));
+            }
+            else{
+                User registeredUser = authenticationService.signup(registerUserDto);
+                return ResponseEntity.ok(new UserResult<>(registeredUser, HttpStatus.OK, CommonConstants.SUCCESS));
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(new UserResult<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, CommonConstants.EXCEPTION), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @RequestMapping(value = RequestMappingConstants.GETTOKEN, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
